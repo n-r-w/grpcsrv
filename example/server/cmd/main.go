38b759971cc7727ea6c4be0a8d9173724d3e6b90
Roman Nikulenkov <nrw@yandex.ru>
2025-02-04 00:17:17 +0300

    ctxlog options

--- example/server/cmd/main.go
+++ example/server/cmd/main.go
@@ -5,12 +5,10 @@ import (
 	"context"
 	"log"
 	"log/slog"
-	"net/http"
 
 	"github.com/n-r-w/ctxlog"
 	srvimpl "github.com/n-r-w/grpcsrv/example/server/implementation"
 	"github.com/n-r-w/grpcsrv/example/telemetry"
-	"google.golang.org/grpc"
 
 	"github.com/n-r-w/bootstrap"
 	"github.com/n-r-w/grpcsrv"
@@ -20,9 +18,15 @@ const serviceName = "greeter-server"
 
 func main() {
 	// Initialize the logger
-	ctx, loggerWrapper, unaryRequestModifier, streamRequestModifier, httpRequestModifier, err := initLogger(serviceName)
+	// Configure and create the logger context
+	ctx, err := ctxlog.NewContext(
+		context.Background(),
+		ctxlog.WithName(serviceName),
+		ctxlog.WithEnvType(ctxlog.EnvProduction),
+		ctxlog.WithLevel(slog.LevelDebug),
+	)
 	if err != nil {
-		log.Fatalf("Failed to initialize logger: %v", err)
+		log.Fatalf("Failed to create logger: %v", err)
 	}
 
 	// Initialize tracing
@@ -37,20 +41,23 @@ func main() {
 	// Create the greeter service
 	initializer := srvimpl.NewGreeterInitializer(&srvimpl.GreeterService{})
 
+	// Get logger options for grpc and http servers
+	opts, err := grpcsrv.GetCtxLogOptions(ctx)
+	if err != nil {
+		log.Fatalf("Failed to get context logger options: %v", err)
+	}
+
 	// Configure and create the gRPC server
-	srv := grpcsrv.New(
-		ctx,
-		[]grpcsrv.IGRPCInitializer{initializer},
+	opts = append(opts,
 		grpcsrv.WithName(serviceName),
 		grpcsrv.WithEndpoint(grpcsrv.Endpoint{
 			GRPC: ":50051",
 			HTTP: ":50052",
 		}),
-		// grpcsrv.WithRecover(),             // enable panic recovery
-		grpcsrv.WithLogger(loggerWrapper), // set external logging interface
-		grpcsrv.WithPprof(),               // enable pprof handlers
-		grpcsrv.WithContextModifiers(unaryRequestModifier, streamRequestModifier, httpRequestModifier),
+		grpcsrv.WithPprof(),
+		// grpcsrv.WithRecover(), // enable panic recovery
 	)
+	srv := grpcsrv.New(ctx, []grpcsrv.IGRPCInitializer{initializer}, opts...)
 
 	// Start the server
 	b, err := bootstrap.New(serviceName)
@@ -60,67 +67,9 @@ func main() {
 
 	err = b.Run(ctx,
 		bootstrap.WithOrdered(srv),
-		bootstrap.WithLogger(loggerWrapper),
+		bootstrap.WithLogger(ctxlog.NewWrapper()),
 	)
 	if err != nil {
 		log.Fatalf("Failed to run bootstrap: %v", err)
 	}
-}
-
-// initLogger initializes the logging system with the given service name.
-// It returns a context with the configured logger, a logger wrapper for external package usage.
-func initLogger(serviceName string) (
-	context.Context, ctxlog.ILogger, grpcsrv.CtxUnaryModifier, grpcsrv.CtxStreamModifier, grpcsrv.CtxHTTPModifier, error,
-) {
-	ctx := context.Background()
-
-	// Configure and create the logger context
-	var err error
-	ctx, err = ctxlog.NewContext(
-		ctx,
-		ctxlog.WithName(serviceName),
-		ctxlog.WithEnvType(ctxlog.EnvProduction),
-		ctxlog.WithLevel(slog.LevelDebug),
-	)
-	if err != nil {
-		return nil, nil, nil, nil, nil, err
-	}
-
-	// Create logger wrapper for other package usage (implement ctxlog.ILogger interface)
-	loggerWrapper := ctxlog.NewWrapper()
-
-	// because we use the logger from ctxlog, which is embedded in the context,
-	// so we have to mix it into the context call of the grpc/http methods
-	injectLoggerToContext := func(
-		ctxRequest context.Context, reqType, method, remoteAddr, traceID string,
-	) context.Context {
-		if ctxlog.InContext(ctxRequest) {
-			return ctxRequest // already injected
-		}
-
-		ctxRequest = ctxlog.ToContextFromContext(ctxRequest, ctx)
-		ctxRequest = ctxlog.With(ctxRequest,
-			"request-type", reqType,
-			"method", method,
-			"remote-addr", remoteAddr,
-			"trace-id", traceID)
-
-		return ctxRequest
-	}
-
-	unaryRequestModifier := func(ctxRequest context.Context, req any, info *grpc.UnaryServerInfo,
-		handler grpc.UnaryHandler, remoteAddr, traceID string,
-	) context.Context {
-		return injectLoggerToContext(ctxRequest, "grpc-unary", info.FullMethod, remoteAddr, traceID)
-	}
-	streamRequestModifier := func(ctxRequest context.Context, info *grpc.StreamServerInfo,
-		handler grpc.StreamHandler, remoteAddr, traceID string,
-	) context.Context {
-		return injectLoggerToContext(ctxRequest, "grpc-stream", info.FullMethod, remoteAddr, traceID)
-	}
-	httpRequestModifier := func(ctxRequest context.Context, r *http.Request, traceID string) context.Context {
-		return injectLoggerToContext(ctxRequest, "http", r.RequestURI, r.RemoteAddr, traceID)
-	}
-
-	return ctx, loggerWrapper, unaryRequestModifier, streamRequestModifier, httpRequestModifier, nil
 }
